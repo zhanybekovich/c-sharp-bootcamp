b9f7c9e800384c3a01a12bb3e205527fb227b819
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-20 20:50:39 +0600

    If else. Logical operators

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -12,14 +12,46 @@ namespace FullCourse
     {
         static void Main(string[] args)
         {
-            // constants
-            const vat = 20;
-            Console.WriteLine(vat);
+            // If statement and logic operators
+            // ==, >, >=, <, <=, !=
+            // ||, &&
 
-            // with var it's needed to initialize variable with value
-            var age = 15;
-            Console.WriteLine(age);
+            int age = 20;
 
+            /*if (age >= 18 && age <= 25)
+            {
+                Console.WriteLine("You are between 18 and 25");
+            } else if (age >= 26)
+            {
+                Console.WriteLine("You are 26 or older");
+            }*/
+
+            /*if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Invalid age");
+            } else
+            {
+                Console.WriteLine("Valid age");
+            }*/
+
+            Console.Write("Enter num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2;
+
+            Console.Write($"Value of {num1} * {num2} is: ");
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+            
+            if (result == userAnswer)
+            {
+                Console.WriteLine("Well done!");
+            } else
+            {
+                Console.WriteLine("Incorrect asnwer!");
+            }
 
             Console.ReadLine();
 
