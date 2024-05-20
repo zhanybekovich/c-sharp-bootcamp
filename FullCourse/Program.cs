8881335d711f1f170f2b2fcfe15f897166fe3d3c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-20 20:57:02 +0600

    Switch statements

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -12,45 +12,34 @@ namespace FullCourse
     {
         static void Main(string[] args)
         {
-            // If statement and logic operators
-            // ==, >, >=, <, <=, !=
-            // ||, &&
+            // Switch statements
 
-            int age = 20;
+            Console.Write("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
 
-            /*if (age >= 18 && age <= 25)
+            switch (day)
             {
-                Console.WriteLine("You are between 18 and 25");
-            } else if (age >= 26)
-            {
-                Console.WriteLine("You are 26 or older");
-            }*/
-
-            /*if (age < 0 || age > 150)
-            {
-                Console.WriteLine("Invalid age");
-            } else
-            {
-                Console.WriteLine("Valid age");
-            }*/
-
-            Console.Write("Enter num1: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter num2: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            int result = num1 * num2;
-
-            Console.Write($"Value of {num1} * {num2} is: ");
-            int userAnswer = Convert.ToInt32(Console.ReadLine());
-            
-            if (result == userAnswer)
-            {
-                Console.WriteLine("Well done!");
-            } else
-            {
-                Console.WriteLine("Incorrect asnwer!");
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Weekend day");
+                    break;
+                default: Console.WriteLine("Invalid day!");
+                    break;
             }
 
             Console.ReadLine();
