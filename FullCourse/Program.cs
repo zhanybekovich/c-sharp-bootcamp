76d28312b11e535da41d7bb168880909fb927e48
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-20 12:15:51 +0600

    Numeric types

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -10,9 +10,41 @@ namespace FullCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Numeric types
+
+            // Integers
+            int age = 20;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            // Long integer
+            long b = 90000000L;
+            Console.WriteLine(b);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            // Decimals
+            // double
+            double c = 55.20;
+            Console.WriteLine(c);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            // float
+            float f = 5.000001F;
+            Console.WriteLine(f);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            // decimal -> money
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
 
             Console.ReadLine();
+
         }
     }
 }
