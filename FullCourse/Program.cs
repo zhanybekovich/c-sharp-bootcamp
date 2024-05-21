c755d0a2a44cbba465f705332bafd0109fb10efe
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-21 20:31:59 +0600

    For loop

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -12,38 +12,18 @@ namespace FullCourse
     {
         static void Main(string[] args)
         {
-            // Switch statements
-
-            Console.Write("Enter a day of the week: ");
-            int day = Convert.ToInt32(Console.ReadLine());
-
-            switch (day)
+            // for loop
+            for (int i = 0; i < 5; i++)
             {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                case 7:
-                    Console.WriteLine("Weekend day");
-                    break;
-                default: Console.WriteLine("Invalid day!");
-                    break;
+                Console.WriteLine($"Hi {i}");
+            }
+
+            for (int i = 0; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
             }
 
             Console.ReadLine();
-
         }
     }
 }
