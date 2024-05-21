1df5864421022705d70bffd29b67c5f03c519750
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-21 21:11:36 +0600

    Ternary operator

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -13,59 +13,11 @@ namespace FullCourse
         static void Main(string[] args)
         {
             // while loop
-            /*int i = 0;
-            while(i < 5)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
+            int age = 10;
 
-            Console.Write("Enter number a: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            string result = age >= 0 ? "Valid" : "Invalid";
 
-            Console.Write("Enter number b: ");
-            int b  = Convert.ToInt32(Console.ReadLine());
-
-            int answer = a * b;
-            int actualAnswer = 0;
-
-            Console.WriteLine($"What is the value of {a} * {b}?");
-
-            while (answer != actualAnswer)
-            {
-                Console.Write("Enter your answer: ");
-                actualAnswer = Convert.ToInt32(Console.ReadLine());
-
-                if (answer != actualAnswer)
-                    Console.WriteLine("Try it again!");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("Well Done!");*/
-
-            // do while
-            Console.Write("Enter number a: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter number b: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            int answer = a * b;
-            int actualAnswer = 0;
-
-            Console.WriteLine($"What is the value of {a} * {b}?");
-
-            do
-            {
-                Console.Write("Enter your answer: ");
-                actualAnswer = Convert.ToInt32(Console.ReadLine());
-
-                if (answer != actualAnswer)
-                    Console.WriteLine("Try it again!");
-                Console.WriteLine();
-            } while (answer != actualAnswer);
-
-            Console.WriteLine("Well Done!");
+            Console.WriteLine(result);
 
             Console.ReadLine();
         }
