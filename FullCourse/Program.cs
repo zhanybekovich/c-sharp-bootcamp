be6731c1f545d471e6511108a2841ef8e7cc1ce1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-22 17:13:35 +0600

    Numeric formatting

--- FullCourse/Program.cs
+++ FullCourse/Program.cs
@@ -12,12 +12,40 @@ namespace FullCourse
     {
         static void Main(string[] args)
         {
-            // while loop
-            int age = 10;
+            // numeric formatting
+            /*double value = 1000D / 12.34D;*/
 
-            string result = age >= 0 ? "Valid" : "Invalid";
+            /*Console.WriteLine(value);
+            Console.WriteLine(string.Format("{0:0}", value));
+            Console.WriteLine(string.Format("{0:0.0}", value));
+            Console.WriteLine(string.Format("{0:0.00}", value));
+            Console.WriteLine(value);*/
 
-            Console.WriteLine(result);
+            double money = 10D / 3D;
+            Console.WriteLine(string.Format("{0:0.00}", money));
+            Console.WriteLine(money.ToString());
+
+            // currency
+            Console.WriteLine(money.ToString("C"));
+            
+            // truncate
+            Console.WriteLine(money.ToString("C0"));
+            
+            // 1 decimal after .
+            Console.WriteLine(money.ToString("C1"));
+
+            // 2 decimal after .
+            Console.WriteLine(money.ToString("C2"));
+
+            Console.WriteLine("============");
+
+            // Providing Culture info
+            // Do not forget to add -> using System.Globalization;
+            Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("ru")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("ky-KG")));
 
             Console.ReadLine();
         }
